03a9267ea4a019e0cd8f615fa3333a190e96a3ec
chuuuul <chuuuul@naver.com>
2018-08-28 21:35:39 +0900

    으아아아 일단날리자

---
+++ Assets/Script/ItemHandler.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.Events;
+
+public class ItemHandler : MonoBehaviour {
+
+    public static List<ItemHandler> ItemhandlerList = new List<ItemHandler>() ;
+
+    public Canvas canvas;
+    internal GraphicRaycaster gr;
+
+
+    public Action<PointerEventData, SlotScript> OnSlotUp { get; set; } //포인터 업 이벤트
+
+    private void Awake()
+    {
+        if ( canvas != null)
+            gr = canvas.GetComponent<GraphicRaycaster>();
+
+    }
+
+
+    public void makeTarget()
+    {
+
+
+    }
+
+
+}
--- Assets/Script/SlotScript.cs
+++ Assets/Script/SlotScript.cs
@@ -1,19 +1,20 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
 
-public class SlotScript : MonoBehaviour,IPointerUpHandler {
+public class SlotScript : MonoBehaviour,IPointerUpHandler,IPointerDownHandler,IPointerEnterHandler,IPointerExitHandler,IPointerClickHandler {
 
     public Stack<ItemScript> itemStack; // 아이템을 넣을 스택
     
     public Sprite blankItemImage;       // 빈 이미지
     public float fontSizeRate = 0.3f;   // 글자 사이즈 비율 ( 슬롯크기와 비례 )
 
+    internal ItemHandler itemHandler;
 
-    
     private Text text;                  // 개수 표현 할 텍스트
     private bool isItemBlank;            // 아이템이 비어있는지 // true : 비어있음 // false : 들어있음
 
@@ -23,7 +24,23 @@ public class SlotScript : MonoBehaviour,IPointerUpHandler {
 
     public bool IsItemBlank_F() { return isItemBlank; }
 
+
     void Start()
+    {
+        setSlot();
+        itemHandler = 
+
+    }
+
+    public void MakeTarget ()
+    {
+        
+
+    }
+
+
+    // 기본창 세팅
+    private void setSlot()
     {
         itemStack = new Stack<ItemScript>();          // 비어있는 스택으로 초기화
         itemImage = transform.GetChild(0).GetComponent<Image>();    // 0번째 자식이 이미지 오브젝트이고
@@ -36,6 +53,19 @@ public class SlotScript : MonoBehaviour,IPointerUpHandler {
         text.fontSize = (int)(size * fontSizeRate);
 
         isItemBlank = true;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        Debug.Log("ww");
+        itemHandler.OnSlotUp.Invoke(eventData, this);
+        
+        if(eventData.button == PointerEventData.InputButton.Left)
+        {
+            Debug.Log("QQ");
+
+        }
+
 
     }
 
@@ -82,6 +112,20 @@ public class SlotScript : MonoBehaviour,IPointerUpHandler {
         // ItemIO.saveDate();                           // 인벤토리에 변동생겼으니 저장?
     }
 
-    
+    public void OnPointerDown(PointerEventData eventData)
+    {
+    }
 
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Debug.Log("ZZZ");
+    }
 }
