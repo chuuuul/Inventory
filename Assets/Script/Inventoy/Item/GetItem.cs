c046f9c45c26b9b29c77e0f4668c0eb8a44d82cc
chuuuul <chuuuul@naver.com>
2018-09-21 04:38:21 +0900

    GetItem if문으로한것 switch로 변경

--- Assets/Script/Inventoy/Item/GetItem.cs
+++ Assets/Script/Inventoy/Item/GetItem.cs
@@ -6,8 +6,8 @@ using System;
 public class GetItem : MonoBehaviour
 {
 
-    
-    public enum MajorTypeEnum { none, commonItem, consum, equipment,  }
+
+    public enum MajorTypeEnum { none, commonItem, consum, equipment, }
     [Header("대분류 설정")]
     public MajorTypeEnum majorType;
 
@@ -28,118 +28,103 @@ public class GetItem : MonoBehaviour
 
     private void Start()
     {
-        
         FindItemWithItemName();
-
     }
 
-    private void ErrorCheck()
+    private void FindItemWithItemName()
     {
-        if (majorType == MajorTypeEnum.none)
-        {
-            Debug.Log(this.gameObject.name);
-            Debug.Log("대분류를 선택하세요");
-            return;
-        }
-
         if (itemName == null)
         {
-
             Debug.Log(this.gameObject.name);
             Debug.Log("아이템 이름을 입력하세요");
             return;
         }
-    }
-    private void FindItemWithItemName()
-    {
-        ErrorCheck();
 
-        if (majorType == MajorTypeEnum.commonItem )
+        switch (majorType)
         {
-            for (int i = 0; i < ItemData.commonItemList.Count; i++)
-            { 
-                if (ItemData.commonItemList[i].Name == itemName)
+            case MajorTypeEnum.none:
                 {
-                    commonItem = ItemData.commonItemList[i];
-                    return;
+                    Debug.Log(this.gameObject.name);
+                    Debug.Log("대분류를 선택하세요");
+                    break;
                 }
-            }
-
-            Debug.Log("CommonItemList에 해당하는 아이템이 없습니다. 아이템 이름을 확인하세요" + itemName);
-            return;
-        }
-
-        if (majorType == MajorTypeEnum.consum)
-        {
-            for (int i = 0; i < ItemData.consumItemList.Count; i++)
-            {
-                if (ItemData.consumItemList[i].Name == itemName)
+            case MajorTypeEnum.commonItem:
                 {
-                    consumItem = ItemData.consumItemList[i];
-                    return;
+                    for (int i = 0; i < ItemData.commonItemList.Count; i++)
+                    {
+                        if (ItemData.commonItemList[i].Name == itemName)
+                        {
+                            commonItem = ItemData.commonItemList[i];
+                            break;
+                        }
+                    }
+
+                    Debug.Log("CommonItemList에 해당하는 아이템이 없습니다. 아이템 이름을 확인하세요" + itemName);
+                    break;
                 }
-            }
-            Debug.Log("ComsumItemList에 해당하는 아이템이 없습니다. 아이템 이름을 확인하세요" +itemName);
-            return;
-        }
 
-
-        if (majorType == MajorTypeEnum.equipment)
-        {
-            for (int i = 0; i < ItemData.equipmentItemList.Count; i++)
-            { 
-                if (ItemData.equipmentItemList[i].Name == itemName)
-                { 
-                    equipmentItem = ItemData.equipmentItemList[i];
-                    return;
+            case MajorTypeEnum.consum:
+                for (int i = 0; i < ItemData.consumItemList.Count; i++)
+                {
+                    if (ItemData.consumItemList[i].Name == itemName)
+                    {
+                        consumItem = ItemData.consumItemList[i];
+                        break;
+                    }
                 }
-            }
-            Debug.Log("EquipmentItemList 해당하는 아이템이 없습니다. 아이템 이름을 확인하세요" + itemName);
-            return;
-        }
+                Debug.Log("ComsumItemList에 해당하는 아이템이 없습니다. 아이템 이름을 확인하세요" + itemName);
+                break;
 
+            case MajorTypeEnum.equipment:
+                for (int i = 0; i < ItemData.equipmentItemList.Count; i++)
+                {
+                    if (ItemData.equipmentItemList[i].Name == itemName)
+                    {
+                        equipmentItem = ItemData.equipmentItemList[i];
+                        break;
+                    }
+                }
+                Debug.Log("EquipmentItemList 해당하는 아이템이 없습니다. 아이템 이름을 확인하세요" + itemName);
+                break;
+        }
     }
 
 
     //충동시 아이템 획득
     private void OnTriggerEnter(Collider other)
     {
-        InventoryTab inventoryTab = null ;
+        InventoryTab inventoryTab = null;
 
-        ErrorCheck();
-
-        if (majorType == MajorTypeEnum.commonItem)
+        switch (majorType)
         {
+            case MajorTypeEnum.commonItem:
+                {
+                    // 드랍될 해당 아이템 수 결정
+                    int dropCount = UnityEngine.Random.Range((int)dropAmountRange.x, (int)dropAmountRange.y + 1);
+                    // 깊은 복사 & 수정
+                    CommonItem item = new CommonItem(dropCount, commonItem.MaxCount, commonItem.BuyPrice, commonItem.SellPrice, commonItem.Id, commonItem.Name, commonItem.DisplayName, commonItem.Description, commonItem.Type, commonItem.Icon);
+                    //아이템이 먹어지는 탭 지정
+                    inventoryTab = TabManager.GetTab(0);
+                    inventoryTab.Add(item, true);
+                    break;
+                }
 
-            // 드랍될 해당 아이템 수 결정
-            int dropCount = UnityEngine.Random.Range((int)dropAmountRange.x, (int)dropAmountRange.y + 1);
-            // 깊은 복사 & 수정
-            CommonItem item = new CommonItem(dropCount, commonItem.MaxCount, commonItem.BuyPrice, commonItem.SellPrice, commonItem.Id, commonItem.Name, commonItem.DisplayName, commonItem.Description, commonItem.Type, commonItem.Icon);
+            case MajorTypeEnum.consum:
+                {
+                    int dropCount = UnityEngine.Random.Range((int)dropAmountRange.x, (int)dropAmountRange.y + 1);
+                    Consum item = new Consum(dropCount, consumItem.MaxCount, consumItem.BuyPrice, consumItem.SellPrice, consumItem.Id, consumItem.Name, consumItem.DisplayName, consumItem.Description, consumItem.Type, consumItem.Icon);
+                    inventoryTab = TabManager.GetTab(0);
+                    inventoryTab.Add(item, true);
+                    break;
+                }
+            case MajorTypeEnum.equipment:
+                {
+                    Equipment item = new Equipment(equipmentItem.BuyPrice, equipmentItem.SellPrice, equipmentItem.Id, equipmentItem.Name, equipmentItem.DisplayName, equipmentItem.Description, equipmentItem.Type, equipmentItem.Icon);
+                    inventoryTab = TabManager.GetTab(0);
+                    inventoryTab.Add(item, true);
+                    break;
+                }
 
-            //아이템이 먹어지는 탭 지정
-            inventoryTab = TabManager.GetTab(0);
-            inventoryTab.Add(item, true);
-        }
-
-        else if (majorType == MajorTypeEnum.consum)
-        {
-
-            // 드랍될 해당 아이템 수 결정
-            int dropCount = UnityEngine.Random.Range((int)dropAmountRange.x, (int)dropAmountRange.y + 1);
-            // 깊은 복사 & 수정
-            Consum item = new Consum(dropCount, consumItem.MaxCount, consumItem.BuyPrice, consumItem.SellPrice, consumItem.Id, consumItem.Name, consumItem.DisplayName, consumItem.Description, consumItem.Type, consumItem.Icon);
-            //아이템이 먹어지는 탭 지정
-            inventoryTab = TabManager.GetTab(0);
-            inventoryTab.Add(item, true);
-        }
-        else if (majorType == MajorTypeEnum.equipment)
-        {
-            // 깊은 복사 & 수정
-            Equipment item = new Equipment(equipmentItem.BuyPrice, equipmentItem.SellPrice, equipmentItem.Id, equipmentItem.Name, equipmentItem.DisplayName, equipmentItem.Description, equipmentItem.Type, equipmentItem.Icon);
-
-            //아이템이 먹어지는 탭 지정
-            inventoryTab = TabManager.GetTab(0);
-            inventoryTab.Add(item, true);
         }
 
         SlotManager.RefreshAll();           //#### 아이템 먹을때는 인벤토리 켜져있을때만 새로고침하면됨
--- Assets/Script/Inventoy/Sub_Function/ContentViewer.cs
+++ Assets/Script/Inventoy/Sub_Function/ContentViewer.cs
@@ -60,7 +60,7 @@ public abstract class ContentViewer : MonoBehaviour {
 
     IEnumerator DisplayViewer(InventorySlot slot)
     {
-
+        yield return null;
 
     }
 
